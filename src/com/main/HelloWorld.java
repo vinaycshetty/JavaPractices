29edffacc64c632db3aa1fd6251e0ca6c6d2d40f
cvinayshetty <cvinayshetty@gmail.com>
2022-01-02 19:14:49 +0530

    commit

---
+++ src/com/main/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.main;
+
+/**
+ * Created By : Vinay Shetty
+ * on : 24/12/21  at 3:48 PM
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello! Welcome to Java World.");
+        System.out.println("new");
+    }
+}
