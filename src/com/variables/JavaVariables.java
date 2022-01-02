0fcca0c7e24718bc184548326f6fae0d8959a86c
cvinayshetty <cvinayshetty@gmail.com>
2022-01-02 23:58:36 +0530

    commit

--- src/com/variables/JavaVariables.java
+++ src/com/variables/JavaVariables.java
@@ -6,9 +6,10 @@ package com.variables;
  **/
 public class JavaVariables {
     public static void main(String[] args) {
-        int a = 10;
-        double b = 10.10;
-        String c = "Vinay";
-        System.out.println("Int : "+a+" Double: "+b+ " String: "+c);
+        int a = 10; // integer datatype stores int value numbers
+        double b = 10.10; // stores decimal values
+        String c = "Vinay"; // store series of words
+        char d = 'x';
+        System.out.println("Int : "+a+" Double: "+b+ " String: "+c +" and Char: "+d);
     }
 }
