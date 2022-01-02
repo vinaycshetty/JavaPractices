5c7269195e81a032e08995bd4da2645fd5bb62a9
cvinayshetty <cvinayshetty@gmail.com>
2022-01-02 20:10:39 +0530

    commit

---
+++ src/com/variables/JavaVariables.java
@@ -0,0 +1,14 @@
+package com.variables;
+
+/**
+ * Created By : Vinay Shetty
+ * on : 02/01/22  at 8:07 PM
+ **/
+public class JavaVariables {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 10.10;
+        String c = "Vinay";
+        System.out.println("Int : "+a+" Double: "+b+ " String: "+c);
+    }
+}
